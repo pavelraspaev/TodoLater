d9d20a6eba48b3d087d14843b4e7164eb163ad03
Pavel Raspaev <pavelraspaev@gmail.com>
2022-05-31 20:05:09 +0300

    Initial Commit

---
+++ TodoLater/TodoLaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoLaterApp.swift
+//  TodoLater
+//
+//  Created by Pavel Raspaev on 31.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct TodoLaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
